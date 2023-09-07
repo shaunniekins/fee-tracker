bf32be35ea81efd949a5948540b9c9fbce7b2c2f
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-09-07 21:51:17 +0800

    Instal dotenv

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+require("dotenv").config();
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -6,3 +7,6 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
+
+console.log("supabaseUrl:", supabaseUrl);
+console.log("supabaseKey:", supabaseKey);
